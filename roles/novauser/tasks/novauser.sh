49681dc318bda0004c6d623afbc0ba3f81540cd8
peaky-peaky <azure84ro@gmail.com>
2021-12-06 17:31:47 +0900

    main

--- roles/novauser/tasks/novauser.sh
+++ roles/novauser/tasks/novauser.sh
@@ -34,6 +34,7 @@ if [ $? -ne 0 ]; then
  mysql -u root -h 7.1.1.20 -e "create database nova_cell0;"
  mysql -u root -h 7.1.1.20 -e "grant all privileges on nova_cell0.* to nova_cell0@'localhost' identified by 'password';"
  mysql -u root -h 7.1.1.20 -e "grant all privileges on nova_cell0.* to nova_cell0@'%' identified by 'password';"
+ mysql -u root -h 7.1.1.20 -e "flush privileges; "
 fi
 
 mysql -u root -h 7.1.1.20 -e "show databases;" > ~/sqlresult
